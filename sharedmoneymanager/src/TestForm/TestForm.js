9c542052eeadea66ed09c8288b36090a76b66d44
Juan Groia <groiajuan@gmail.com>
2023-09-21 14:30:42 +0200

    Primary Form
    Primary Form

--- sharedmoneymanager/src/TestForm/TestForm.js
+++ sharedmoneymanager/src/TestForm/TestForm.js
@@ -1,17 +1,78 @@
 import React from "react";
+import { useFormik } from "formik";
 
-function TestForm()
-{
-    return (
-        <div className="formClass">           
+function TestForm() {
+    
+    const validate = values => {
+
+        const errors = {};
         
-       <h2>Este es el form</h2>
-       </div> 
+        if (!values.firstName) {
+          errors.firstName = 'Required';
+        } else if (values.firstName.length > 15) {
+          errors.firstName = 'Must be 15 characters or less';
+        }
+      
+        if (!values.lastName) {
+          errors.lastName = 'Required';
+        } else if (values.lastName.length > 20) {
+          errors.lastName = 'Must be 20 characters or less';
+        }
+      
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+          errors.email = 'Invalid email address';
+        }
+      
+        return errors;
+      };
 
-           );
 
+    const formik = useFormik({
+        initialValues: {
+          firstName: '',
+          lastName: '',
+          email: '',
+        },
+        validate,
+        onSubmit: values => {
+          alert(JSON.stringify(values, null, 2));
+        },
+      });
 
-};
-
+      return (
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.firstName}
+          />
+    
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.lastName}
+          />
+    
+          <label htmlFor="email">Email Address</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+    
+          <button type="submit">Submit</button>
+        </form>
+      );
+    };
 
 export default TestForm;
