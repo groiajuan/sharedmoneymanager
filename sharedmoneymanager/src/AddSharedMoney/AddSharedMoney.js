0b64cb093369d7cd415fd5e6886695f6e813953f
Juan Groia <groiajuan@gmail.com>
2023-09-21 14:06:05 +0200

    Initial Form
    Initial Form

---
+++ sharedmoneymanager/src/AddSharedMoney/AddSharedMoney.js
@@ -0,0 +1,19 @@
+import React from "react";
+import TestForm from "../TestForm/TestForm";
+
+function AddSharedMoney()
+{
+    return (
+        <div id="parentcontainter">           
+        
+       <h1>Holaa aca se viene el form</h1>
+       <TestForm/>
+       </div> 
+
+           );
+
+
+};
+
+
+export default AddSharedMoney;
---
+++ sharedmoneymanager/src/TestForm/TestForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function TestForm()
+{
+    return (
+        <div className="formClass">           
+        
+       <h2>Este es el form</h2>
+       </div> 
+
+           );
+
+
+};
+
+
+export default TestForm;
